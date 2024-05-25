1b6168dbbc5c19ca94a470e17bbf52facc9bdcae
sonal <sonalsingh4102@gmail.com>
2024-05-25 15:56:35 +0530

    gradient color add

--- components/Banner.jsx
+++ components/Banner.jsx
@@ -2,14 +2,14 @@ import React from "react";
 
 function Banner() {
 	return (
-		<div className="bg-primary">
-			<div className=" text-white h-screen lg:h-[86vh] 2xl:h-[90vh] flex items-center justify-between container mx-auto px-4 lg:w-[80%] 2xl:w-[80%] space-x-12 ">
+		<div className=" bg-gradient-to-b from-primary via-primary/95 to-primary/75">
+			<div className=" text-white h-[90vh] lg:h-[86vh] 2xl:h-[90vh] flex items-center justify-between container mx-auto px-4 lg:w-[80%] 2xl:w-[80%] space-x-12 ">
 				<div className="lg:w-[50%] flex flex-col space-y-12 lg:my-0 my-12">
 					<div className="flex flex-col space-y-5">
-						<h1 className="lg:text-left text-center text-4xl lg:text-5xl font-medium lg:leading-snug leading-snug">
+						<h1 className="lg:text-left text-center text-5xl font-medium lg:leading-snug leading-snug">
 							Automate your hiring process
 						</h1>
-						<p className="text-sm lg:text-lg lg:text-left text-center ">
+						<p className="text-lg lg:text-left text-center ">
 							JobScout helps your team transcribe, summarize,
 							search, and analyze voice conversations.
 						</p>
