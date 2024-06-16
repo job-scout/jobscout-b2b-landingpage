ae3bde7cc7201694a4b315c6406da69722016140
jagdishpal02001 <155366813+jagdishpal02001@users.noreply.github.com>
2024-06-16 10:31:59 +0530

    update pipeline

--- pages/_app.js
+++ pages/_app.js
@@ -11,7 +11,7 @@ export default function App({ Component, pageProps }) {
 	return (
 		<>
 			<Head>
-				<title>Jobscout Website</title>
+				<title>Jobscout | Accelerate Your Hiring Process with AI</title>
 				<link
 					rel="apple-touch-icon"
 					href="/favicon.ico"
